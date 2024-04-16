94ad368543e64abcd9b5b54d34b691ab678ea54d
Feys Dylan <148909357+HappyFeys@users.noreply.github.com>
2024-04-16 14:34:29 +0200

    Ajout d'une seconde méthode pour le dernier exercice

--- 3.Create/app.js
+++ 3.Create/app.js
@@ -31,53 +31,35 @@ const learner = ["Tom",
     "Dorian"];
 
 const article = document.querySelector('article');
+const titre = document.querySelector('h2');
 console.log("learner lenght :"+learner.length);
 
-// Fonction pour retourner un tableau mélangé
-
-function randomLearner(array) {
-    const shuffledArray = array.slice();
-    randomArray(shuffledArray);
-    console.log("mélange length : "+shuffledArray.length);
-    console.log(shuffledArray);
-    const learnerBis = [];
-    while(shuffledArray.length>0){
-        const apprenant = shuffledArray.splice(0,1)[0];
-        learnerBis.push(apprenant);
-    }
-    return learnerBis;
-}
-
-const learnerRandom = randomLearner(learner);
-
-// Fonction pour mélanger le tableau
-function randomArray(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-}
-
-
-for (const element of learnerRandom) {
+for (const element of learner) {
     let section = document.createElement('section');
+    //Attribution d'une BGC aléatoire à la section
     section.style.backgroundColor=`rgb(${randomColor()},${randomColor()},${randomColor()})`;
-   
+    //Création d'un p avec le nom des apprenants
     let name = document.createElement('p');
     name.innerText=element;
+    //On ajoute notre p à la section
     section.appendChild(name);
-    article.appendChild(section);
+    //Pour chaque étudiant on cherche un nombre entre 0 et 1, si l'élève à 1, on le place sous le h2, si pas on le place a la suite de la section.
+    let numberRandom = Math.floor(Math.random()*2);
+    console.log(numberRandom);
+    (numberRandom==1) ?
+        titre.insertAdjacentElement('afterend',section) :
+        article.appendChild(section);
+    //Ici, on recupère le code rgb de section
     let couleurDeFond = getComputedStyle(section).backgroundColor;
+    //On utilise un RegEx pour ne récupérer que les chiffres
     let rgbValue = couleurDeFond.match(/\d+/g);
     let rouge = parseInt(rgbValue[0]);
     let vert = parseInt(rgbValue[1]);
     let bleu = parseInt(rgbValue[2]);
-
+    //Si la somme de ces valeurs supérieurs ou égal à 128, considérée comme light donc on met une couleur black, sinon on met blanc
     if (((0.3*rouge)+(0.59*vert)+(0.11*bleu))>=128) {
         name.style.color="black";
     } else{
         name.style.color="white";
-    }
-}
-
+    } 
+} 
